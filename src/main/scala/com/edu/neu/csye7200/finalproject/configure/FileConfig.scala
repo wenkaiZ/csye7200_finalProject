84dec3c50e40b81b16e4185827b5e9fa07b3fc7d
sixin <wang.sixi@husky.neu.edu>
2019-12-06 01:59:33 -0500

    structure refactor

---
+++ src/main/scala/com/edu/neu/csye7200/finalproject/configure/FileConfig.scala
@@ -0,0 +1,7 @@
+package com.edu.neu.csye7200.finalproject.configure
+
+object FileConfig {
+  val dataDir = "data/"
+  val appFile = dataDir + "app_info.csv"
+  val ratingFile = dataDir + "ratings.csv"
+}
