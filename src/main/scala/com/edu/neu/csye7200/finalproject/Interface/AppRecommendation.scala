562218ceef8e3c3f5ca547976f684d8e3c2c59f7
Wenkai Zheng <zheng.wenk@husky.neu.edu>
2019-12-06 04:29:30 -0500

    Whole system strcture

--- src/main/scala/com/edu/neu/csye7200/finalproject/Interface/AppRecommendation.scala
+++ src/main/scala/com/edu/neu/csye7200/finalproject/Interface/AppRecommendation.scala
@@ -1,20 +1,121 @@
 package com.edu.neu.csye7200.finalproject.Interface
 
+import java.sql.Date
+
 import com.edu.neu.csye7200.finalproject.configure.FileConfig
-import com.edu.neu.csye7200.finalproject.util.DataUtil
+import com.edu.neu.csye7200.finalproject.util.{ALSUtil, DataUtil, QueryUtil}
+import com.github.tototoshi.csv._
+
 
 object AppRecommendation {
-  lazy val appDF = DataUtil.getAppDF
+  lazy val df=DataUtil.getAppsDF
 
   /**
-    * This function trained the data and get the recommendation movie
+    * This function trained the data and get the recommendation application
     * for specific user and print the result.
     * @param userId     The specific user of recommendation
     * @return           Return the RMSE and improvement of the Model
     */
-  def getRecommendation(userId: Int) ={
+  def getRecommendation(userId: Int) = {
+    //RDD[long, Rating]
     val ratings = DataUtil.getAllRating(FileConfig.ratingFile)
 
+    val appsArray = DataUtil.getAppsArray
+
+    val links = DataUtil.getLinkData(FileConfig.linkFile)
+    val apps = DataUtil.getCandidatesAndLink(appsArray, links)
+
+    val userRatingRDD = DataUtil.getRatingByUser(FileConfig.ratingFile, userId)
+    val userRatingApp = userRatingRDD.map(x => x.product).collect
+      val numRatings = ratings.count()
+      val numUser = ratings.map(_._2.user).distinct().count()
+      val numApp = ratings.map(_._2.product).distinct().count()
+
+      println("rating: " + numRatings + " apps: " + numApp
+        + " user: " + numUser)
+
+      // Split data into train(60%), validation(20%) and test(20%)
+      val numPartitions = 10
+
+      val trainSet = ratings.filter(x => x._1 < 6).map(_._2).
+        union(userRatingRDD).repartition(numPartitions).persist()
+      val validationSet = ratings.filter(x => x._1 >= 6 && x._1 < 8)
+        .map(_._2).persist()
+      val testSet = ratings.filter(x => x._1 >= 8).map(_._2).persist()
+
+      val numTrain = trainSet.count()
+      val numValidation = validationSet.count()
+      val numTest = testSet.count()
+
+      println("Training data: " + numTrain + " Validation data: " + numValidation
+        + " Test data: " + numTest)
+
+      //      Train model and optimize model with validation set
+      ALSUtil.trainAndRecommendation(trainSet, validationSet, testSet, apps, userRatingRDD)
+    }
+
+    def queryBySelectedInAppsJson(content: String, SelectedType: String) = {
+      QueryUtil.QueryAppJson(df, content, SelectedType)
+    }
+
+    def queryBySeletedInAppsNormal(content: String, SelectedType: String) = {
+      QueryUtil.QueryAppInfoNorm(df, content, SelectedType)
+    }
+
+    def queryByKeywords(content: String) = {
+      //Query of keywords
+      val keywordsRDD = DataUtil.getKeywords(FileConfig.keywordsFile)
+      QueryUtil.QueryOfKeywords(keywordsRDD, df, content)
+    }  
+
+  def SortBySelected(ds:Array[(Int,String,String,String,Date,Double)],selectedType:String="popularity",order:String="desc")= {
+    selectedType match {
+      case "popularity" => order match {
+        case "desc" => ds.sortBy(-_._6)
+        case "asc" => ds.sortBy(_._6)
+      }
+      case "release_date" =>
+        order match {
+          case "desc" => ds.sortWith(_._5.getTime > _._5.getTime)
+          case _ => ds.sortBy(-_._5.getTime)
+        }
+    }
   }
 
-}
+    def queryBystaffInCredits(content: String, SelectedType: String) = {
+      QueryUtil.QueryOfstaff(DataUtil.getStaff(FileConfig.creditFIle), df, content, SelectedType)
+    }
+
+  /**
+    * Search apps by movie name
+    * @param AppName    The user input of app name
+    * @return             Option of Array of [Int] contains the app tmdbID
+    */
+    def FindAppByName(AppName: String) = {
+      val id = QueryUtil.QueryAppIdByName(df, AppName).map(x => x._1)
+      if (id.length != 0)
+        Some(id)
+      else None
+    }
+
+
+    def UpdateRatingsByRecommendation(RatingsInfo: List[String], AppName: String) = {
+      val appId = FindAppByName(AppName).getOrElse(Array())
+      val writer = CSVWriter.open(FileConfig.ratingFile, append = true)
+      if (appId.nonEmpty) {
+        val links = DataUtil.getLinkData(FileConfig.linkFile)
+        val imdbId = DataUtil.movieIdTransfer(appId, links)
+        writer.writeRow(insert(RatingsInfo, 1, imdbId(0)))
+        println("Rating Successfully")
+      }
+      else println("Cannot find the app you entered")
+      writer.close()
+
+    }
+
+    def insert(list: List[String], i: Int, value: Int) = {
+      list.take(i) ++ List(value.toString) ++ list.drop(i)
+    }
+  }
+
+
---
+++ src/main/scala/com/edu/neu/csye7200/finalproject/Main.scala
@@ -0,0 +1,150 @@
+package com.edu.neu.csye7200.finalproject
+import com.edu.neu.csye7200.finalproject.Interface.AppRecommendation
+
+import scala.util.control.Breaks._
+import scala.util.{Failure, Success, Try}
+
+object Main extends App {
+  def ToInt(line: String): Try[Int] = {
+    Try(line.toInt)
+  }
+
+  override def main(args: Array[String]): Unit = {
+    breakable {
+      while (true) {
+        println(
+          "\n/-----------------------------------------------------------------------/" +
+          "\n/-------------------- Welcome to AppRecommendation ---------------------/" +
+          "\n/-----------------------------------------------------------------------/" +
+          "\n/-------------------- Please Log In By Enter UserId --------------------/" +
+          "\n/-----------------------------------------------------------------------/" +
+          "\n/------------------------- Input exit to Exit --------------------------/"
+        )
+        val id = scala.io.StdIn.readLine()
+        if (id.equals("exit")) break
+        breakable {
+          while (true) {
+            ToInt(id) match {
+              case Success(t) => {
+
+                println(
+                    "\n/-----------------------------------------------------------------------/" +
+                    "\n/-------------------- Welcome to AppRecommendation ---------------------/" +
+                    "\n/-----------------------------------------------------------------------/" +
+                    "\n                             User ID:  " + id +
+                    "\n/-----------------------------------------------------------------------/" +
+                    "\n/------------------------- Input exit to Exit --------------------------/"
+                )
+                println("1.App Recommendation" +
+                  "\n2.App Search" +
+                  "\n3.App Rating(give better recommendation for you)")
+                var  num =scala.io.StdIn.readLine()
+                if(num.equals("exit")) break
+                breakable {
+                  while (true) {
+                    ToInt(num) match {
+                      case Success(t)=>{
+                        t match{
+                          case 1=>{ println(
+                              "\n/-----------------------------------------------------------------------/" +
+                              "\n/-------------------- Welcome to AppRecommendation ---------------------/" +
+                              "\n/-----------------------------------------------------------------------/" +
+                              "\n                             User ID:  " + id +
+                              "\n/-----------------------------------------------------------------------/" +
+                              "\n/------------------------- Input exit to Exit --------------------------/"
+                          )
+                            AppRecommendation.getRecommendation(id.toInt)
+                            break
+                          }
+                          case 2=>{
+                            breakable {
+                              while (true) {
+                                println(
+                                  "\n/-----------------------------------------------------------------------/" +
+                                  "\n/------------------------- App Search ----------------------------------/" +
+                                  "\n/-----------------------------------------------------------------------/" +
+                                  "\n                             User ID:  " + id +
+                                  "\n/-----------------------------------------------------------------------/" +
+                                  "\n/--------------------- Input exit to Exit ------------------------------/"
+                                )
+                                println("1.Search by Keywords" +
+                                  "\n2.Search by production_coutries" +
+                                  "\n3.Search by name"
+                                )
+                                num = scala.io.StdIn.readLine()
+                                if(num.equals("exit")) break
+                                ToInt(num) match {
+
+                                  case Success(v) => {
+                                    println("Enter Content")
+                                    val content=scala.io.StdIn.readLine()
+                                    v match{
+                                      case 1 => AppRecommendation.queryByKeywords(content).sortWith(_._6 > _._6).take(2).foreach(line => println("id: " + line._1, "popularity: " +
+                                        line._6, "name: " + line._3, "keywords: " + line._2, "tagline: " + line._4, "release_date: " + line._5))
+                                      case 2 => AppRecommendation.queryBySelectedInAppsJson(content, "production_countries").sortWith(_._6 > _._6).take(2).foreach(line => println("id: " + line._1, "popularity: " +
+                                        line._6, "name: " + line._3, "production_coutries: " + line._2, "tagline: " + line._4, "release_date: " + line._5))
+                                      case 3 => AppRecommendation.queryBySeletedInAppsNormal(content, "title").sortWith(_._6 > _._6).take(2).foreach(line => println("id: " + line._1, "popularity: " +
+                                        line._6, "name: " + line._2, "tagline: " + line._4, "release_date: " + line._5))
+                                      case _=>break
+                                    }
+
+                                  }
+                                  case Failure(e)=> break
+                                }
+                              }
+                            }
+                          }
+                          case 3=>{
+                            println(
+
+                                "\n/-----------------------------------------------------------------------/" +
+                                "\n/------------------------- App Rating ----------------------------------/" +
+                                "\n/-----------------------------------------------------------------------/" +
+                                "\n                             User ID:  " + id +
+                                "\n/-----------------------------------------------------------------------/" +
+                                "\n/---------------- Enter the App name you wanna rate --------------------/" +
+                                "\n/-----------------------------------------------------------------------/" +
+                                "\n/--------------------- Input exit to Exit ------------------------------/"
+                            )
+                            val content=scala.io.StdIn.readLine()
+                            if(content.equals("exit")) break
+                            breakable {
+                              while (true) {
+                                println("Enter rating you wanna give(0~5)")
+                                val rating=scala.io.StdIn.readLine()
+                                if(rating.equals("exit")) break
+                                Try(rating.toFloat) match{
+                                  case Success(r)=> {
+                                    if(r>=0&&r<=5) {
+                                      AppRecommendation.UpdateRatingsByRecommendation(List(id.toInt.toString, r.toString,
+                                        (System.currentTimeMillis()%10000000000.00).toLong.toString), content)
+                                      break
+                                    }
+                                    else {
+                                      println("out of range")
+                                    }
+                                  }
+                                  case Failure(r)=>
+                                }
+
+                              }
+                            }
+                          }
+                          case _=>break
+                        }
+
+                      }
+                      case Failure(e)=>break
+                    }
+                  }
+                }
+              }
+              case Failure(e)=> break
+            }
+
+          }
+        }
+      }
+    }
+  }
+}
